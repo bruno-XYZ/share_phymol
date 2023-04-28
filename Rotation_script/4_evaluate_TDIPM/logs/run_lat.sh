bd7de6ba9d41f88a88e4bb79b23c9c7de640e8e1
bruno_vb <113997970+bruno-XYZ@users.noreply.github.com>
2023-04-28 12:43:14 +0200

    Delete Rotation_script directory

--- Rotation_script/4_evaluate_TDIPM/logs/run_lat.sh
+++
@@ -1,8 +0,0 @@
-for i in `ls -d C*/`; do
-    cd $i
-        cp *.tex *_tex
-        cd *_tex
-            xelatex *tex 
-        cd ..
-    cd ..
-done
--- Rotation_script/4_evaluate_TDIPM/logs/script_run.sh
+++
@@ -1,6 +0,0 @@
-for i in `ls -d C*/`; do 
-    cd $i
-        ../../script_evaluate_latex_out.py ${i%/}
-    cd ..
-done
-
